fd38badb29ab7fd00042fc97ffd992b6f8171687
unknown <paortegon@uniboyaca.edu.co>
2023-04-22 21:48:21 -0500

    Se agrego el ejercicio 4

--- js/ejercicio.js
+++ js/ejercicio.js
@@ -38,6 +38,26 @@ document.getElementById('btn_idEdad')
     }
     }
 });
+//ejercicio 4
+const btnVerificar = document.getElementById("verificar");
+const btnVolver = document.getElementById("volver");
+
+btnVerificar.addEventListener("click", function() {
+    const nombre = document.getElementById("nombre").value;
+    const materia = document.getElementById("materia").value;
+    const nota1 = parseFloat(document.getElementById("nota1").value);
+    const nota2 = parseFloat(document.getElementById("nota2").value);
+    const nota3 = parseFloat(document.getElementById("nota3").value);
+
+    const promedio = (nota1 + nota2 + nota3) / 3;
+    if (promedio >= 3.0) {
+        document.getElementById("resultado").style.color = "green";
+        document.getElementById("resultado").textContent = 'Felicitaciones '+nombre+', su nota es '+promedio.toFixed(2)+' pasaste la materia '+materia;
+    } else {
+        document.getElementById("resultado").style.color = "red";
+        document.getElementById("resultado").textContent = 'Lo siento '+nombre+', su nota es '+promedio.toFixed(2)+', perdiste la materia '+materia;
+    }
+});
 
 //ejercicio 8
 document.getElementById('btn_idNum')
