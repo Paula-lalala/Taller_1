38eb8d05b755af354bcaee1a63141a1611776d45
unknown <paortegon@uniboyaca.edu.co>
2023-04-22 20:30:23 -0500

    creacion de la carpeta js, html del ejercicio 2 y creacion del archivo ejercicio.js

---
+++ js/ejercicio.js
@@ -0,0 +1,19 @@
+// ejercicio 1
+
+function countChars(obj){
+    document.getElementById("charNum").innerHTML = obj.value.length+' caracteres';
+}
+
+let texto= document.getElementById('btn_identificar')
+.addEventListener('click', function () {
+    let verTexto = document.getElementById('verTexto').value;
+    if (!verTexto) {
+        document.getElementById("idTexto").innerText = ("Debe ingresar un valor");
+    } else {
+        if (!isNaN(verTexto)) {
+            document.getElementById("idTexto").innerText = ("Es un numero");
+        }else{
+            document.getElementById("idTexto").innerText = ("Es un texto"); 
+        }
+    }
+});
